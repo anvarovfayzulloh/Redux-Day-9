6ef9a928fa888af1f5c1d30cc8af654744536b97
anvarovfayzulloh <fayzee200708@gmail.com>
2024-09-15 00:00:08 +0300

    finish

--- src/components/singleProduct/SingleProduct.jsx
+++ src/components/singleProduct/SingleProduct.jsx
@@ -4,6 +4,16 @@ import { useParams } from 'react-router-dom';
 import { useGetProductQuery } from '../../redux/api/productApi';
 import { Carousel } from 'antd';
 
+
+const contentStyle = {
+    margin: 0,
+    height: '160px',
+    color: '#fff',
+    lineHeight: '160px',
+    textAlign: 'center',
+    background: '#364d79',
+  };
+
 const SingleProduct = () => {
     const { id } = useParams();
     const { data } = useGetProductQuery(id);
