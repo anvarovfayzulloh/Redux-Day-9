a06d7a60312d5fd9fa1996abe225488c782f9062
anvarovfayzulloh <fayzee200708@gmail.com>
2024-09-17 18:09:26 +0300

    finish

--- src/routes/profile/Profile.jsx
+++ src/routes/profile/Profile.jsx
@@ -9,7 +9,7 @@ const Profile = () => {
   const profile = data?.data?.payload;
   const dispatch = useDispatch();
 
-  const handleLogOut= () => {
+  const handleLogOut = () => {
     dispatch(logOut())
   }
 
@@ -17,9 +17,9 @@ const Profile = () => {
     <div className="bg-black min-h-screen py-12">
       <div className="max-w-[1000px] mx-auto px-8">
         {profile ? (
-          <div className="flex items-center shadow-md rounded-lg overflow-hidden bg-gray-800">
-            <div className="p-6 flex items-center justify-center bg-gray-900">
-              <img src={profile.photo_url}  className="w-[120px] h-[120px] rounded-full object-cover shadow-md"/>
+          <div className="flex  shadow-md rounded-lg overflow-hidden bg-gray-800">
+            <div className="px-6  flex items-center justify-center bg-gray-900">
+              <img src={profile.photo_url} className="w-[120px] h-[120px] rounded-full object-cover shadow-md" />
             </div>
             <div className="flex-1 p-6 text-white">
               <h1 className="text-3xl font-semibold mb-2">Welcome, {profile.first_name}!</h1>
@@ -27,7 +27,7 @@ const Profile = () => {
               <p className="text-lg mb-3">Role: <span className="font-normal text-gray-300 capitalize">{profile.role}</span></p>
               <button className="px-6 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 transition duration-300" onClick={handleLogOut}>
                 Logout
-                </button>
+              </button>
             </div>
           </div>
         ) : (
